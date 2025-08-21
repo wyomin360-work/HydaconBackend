0da107aadfb8791a1f1c02661fc2dfb2ff152068
PRINCE ROY <prince@woi360.com>
2025-08-21 11:28:07 +0530

    feat: add image upload with common module

---
+++ src/middlewares/multer.js
@@ -0,0 +1,17 @@
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        const uploadPath = path.join(__dirname, '../uploads/images')
+        cb(null, uploadPath)
+    },
+    filename: (req, file, cb) => {
+        let trimmedName = file?.originalname?.trim()?.split(' ')?.join('_')        
+        cb(null, Date.now() + '-' + trimmedName)
+    }
+})
+
+const upload = multer({ storage })
+
+module.exports = upload
---
+++ src/modules/common/common.paths.js
@@ -0,0 +1,4 @@
+module.exports = {
+    root: '/service',
+    imageUpload: '/upload-image'
+}
