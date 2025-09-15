da380b0721ad66a18bf02ff695c047214613a310
PRINCE ROY <prince@woi360.com>
2025-09-15 14:35:26 +0530

    fix: fix notifications messages

--- src/modules/redeems/redeems.service.js
+++ src/modules/redeems/redeems.service.js
@@ -1,4 +1,5 @@
 const { REDEEM_STATUS, LIGHT_CARD_COLORS } = require('../../constants/redeem')
+const { APP_NOTIFICATIONS } = require('../../constants/notifications')
 const { sendFcmNotifications } = require('../../functions/fcm')
 const Product = require('../../schemas/product.schema')
 const Redeem = require('../../schemas/redeem.schema')
