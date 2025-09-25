f0c49b66e6e47310df374760fa442c1a6069512d
PRINCE ROY <prince@woi360.com>
2025-09-25 15:07:52 +0530

    refactor: update nodemailer port to 2525

--- src/functions/nodemailer.js
+++ src/functions/nodemailer.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   host: "smtp.sendgrid.net",
-  port: 587,
+  port: 2525,
   secure: false,
   auth: {
     user: 'apikey',
